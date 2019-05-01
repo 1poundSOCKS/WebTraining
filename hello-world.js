e3f45bc4d02f2cd4b687d67b8b13b67d993dcb9d
1poundSOCKS <mathew.coburn@gmail.com>
2019-05-01 23:03:23 +0100

    Various

--- hello-world.js
+++ hello-world.js
@@ -9,7 +9,7 @@ const MongoURL = "mongodb://localhost:27017/test"
 
 const port = process.env.PORT || 3000
 
-// read an existing user's data by ID
+// read an existing user's data by id
 app.get('/api/users/:id', (req, res) => {
 
   try {
@@ -47,18 +47,62 @@ app.get('/api/users/:id', (req, res) => {
   }
 })
 
-// create a new user
-app.post('/api/users', (req, res) => {
+// read an existing user's data by email
+app.get('/api/users/findbyemail/:email', (req, res) => {
+
+  try {
+    const email = req.params.email
+
+    MongoClient.connect(MongoURL, (err,db) => {
+      if ( err ) {
+        console.log(`Failed to connect to MongoDB '${MongoURL}'`)
+        res.status(500).send("Failed to retrieve user")
+      }
+      else {
+        console.log(`Connected to MongoDB '${MongoURL}'`)
+        db.db("test").collection("users").findOne({email: `${email}`}, (err, result) => {
+          if ( err ) {
+            console.log(`ERROR: MongoDB findOne failed`)
+            console.log(`${err}`)
+            res.status(500).send({ status: "ERROR", message: "MongoDB error"})
+          }
+          else {
+            if ( result ) {
+                console.log(`User found`)
+                res.send({ status: "SUCCESS", result })
+              }
+              else {
+                console.log(`ERROR: User not found`)
+                res.send({ status: "ERROR", message: "User not found"})
+              }
+          }
+        })
+      }
+    })
+  }
+  catch( e ) {
+    res.status(500).send({ status: "ERROR", message: `${e}`})
+  }
+})
+
+function ValidateRequest(req, callback) {
 
   const Joi = require('joi')
   const any = Joi.any()
 
   const schema = Joi.object().keys({
     forename: Joi.string(),
-    surname: Joi.string()
+    surname: Joi.string(),
+    email: Joi.string()
   })
 
-  const result = Joi.validate(schema, req.body, (err, value) => {
+  const result = Joi.validate(schema, req.body, callback)
+}
+
+// create a new user
+app.post('/api/users', (req, res) => {
+
+  ValidateRequest(req, (err, value) => {
     if ( err ) {
       res.status(422).json({
         status: "ERROR",
@@ -89,23 +133,15 @@ app.post('/api/users', (req, res) => {
             }
           })
         }
-      })
+      });
     }
   })
 })
 
 // update an existing user
-app.put('/api/users/:id', (req, res) => {
+app.put('/api/users/:email', (req, res) => {
 
-  const Joi = require('joi')
-  const any = Joi.any()
-
-  const schema = Joi.object().keys({
-    forename: Joi.string().alphanum().required(),
-    surname: Joi.string().alphanum().required()
-  })
-
-  const result = Joi.validate(schema, req.body, (err, value) => {
+  ValidateRequest(req, (err, value) => {
     if ( err ) {
       res.status(422).json({
         status: "ERROR",
