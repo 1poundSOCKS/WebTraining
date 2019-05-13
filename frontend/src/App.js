c2fb62719feb964366d38b20fdeb36730b3d2606
1poundSOCKS <mathew.coburn@gmail.com>
2019-05-13 22:23:01 +0100

    Adding logon to front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Logon from './components/logon.js';
 
 function App() {
   return (
     <div className="App">
-      <h1></h1>
+      <Logon/>
     </div>
   );
 }
