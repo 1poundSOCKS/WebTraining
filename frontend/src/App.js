e27d4f90e44a64c212fda98c08e95cdddcc8aefd
1poundSOCKS <mathew.coburn@gmail.com>
2019-05-26 23:07:04 +0100

    Attempt to add cookies for logon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
+import { Cookies } from 'react-cookie';
 import Logon from './components/logon.js';
 
 function App() {
+
+  //var logon = Cookies.get('logon');
+
   return (
     <div className="App">
       <h1>Web Test</h1>
-      <Logon/>
+          <Logon/>
     </div>
   );
 }
