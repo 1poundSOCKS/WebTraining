e04717c858d7608258471d43eef3d0a3c8fe07eb
1poundSOCKS <mathew.coburn@gmail.com>
2019-12-09 21:23:02 +0000

    Added YouTube embedded vids

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { Cookies } from 'react-cookie';
 import Logon from './components/logon.js';
 
 import titlePic from'./assets/miguels.jpg';
+import nextPic from'./assets/sic_pitch.jpg';
 
 function App() {
 
@@ -13,9 +14,11 @@ function App() {
   if( !logon ) {
     return (
       <div className="App">
-        <h1>Red River Gorge</h1>
         <p1>
-        <img src={titlePic} alt=""/>
+        <h1>Why we don't have Free Will & Why that's OK</h1>
+        <iframe title="Why we don't have Free Will & Why that's OK" width="100%" height="315" src="https://www.youtube.com/embed/o0GN4urbA_c"></iframe>
+        <h1>The Story of Fat: Why we were Wrong about Health</h1>
+        <iframe title="The Story of Fat: Why we were Wrong about Health" width="100%" height="315" src="https://www.youtube.com/embed/5S6-v37nOtY"></iframe>
         </p1>
       </div>
     );
